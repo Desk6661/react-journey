a100284826883ccd97e3bcbc14908ac2eae699e9
Desk6661 <sadhudip212@gmail.com>
2025-10-20 13:06:30 +0530

    used scrima for my first project!!

---
+++ first-project/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="/images/react-logo.png" alt="React logo" />
+                <img src="/images/react-logo-half.png" alt="React logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
